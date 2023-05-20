5ed1f7226251c4aa417e0a52dc8e2502fe0b1aac
Shubham Tribhuvan <shubhamtribhuvan8@gmail.com>
2023-05-20 21:44:54 +0530

    firebase sdk chatengine integration secret key aaded

--- realtimechataapp/src/App.js
+++ realtimechataapp/src/App.js
@@ -1,8 +1,17 @@
-import "./App.css";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./Component/Login";
+import { AuthProvider } from "./contexts/AuthContext";
+import Chats from "./Component/Chats";
 function App() {
   return (
-    <div className="App">
-      <h1>Helllo Chat App</h1>
+    <div style={{ fontFamily: "Avenir" }}>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/chats" element={<Chats />} />
+        </Routes>
+      </AuthProvider>
     </div>
   );
 }
